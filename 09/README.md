e523358f10e8b034ed15bcd9fe0ae492457417ce
Adrien Lucbert <adrien.lucbert@epitech.eu>
2020-04-29 23:17:48 +0200

    [ADD] course 09: Index Buffers in OpenGL

---
+++ 09/README.md
@@ -0,0 +1,59 @@
+# Episode 09: Index Buffers in OpenGL
+
+Every shape in video games is made out of triangles. Wanna draw a square? Draw 
+2 triangles side by side. But when doing so, 2 vertices are repeated, consuming 
+memory space worthlessly.
+
+```
+3      2
+o------o
+|    / |
+|  /   |
+|/     |
+o------o
+0      1
+```
+
+To draw this shape, we may use 6 vertices
+- first triangle: { 0, 1, 2 }
+- second triangle: { 2, 3, 0 }
+By doing so, if we define 3 vertices for our first triangle, and 3 vertices for 
+our second triangle, vertices 0 and 2 are duplicated. We don't want to waste 
+GPU memory.
+
+To solve this, we can make index buffers. Our vertex buffer will contain all our 
+vertices, each designated by an index. Our index buffer will contain which 
+vertices (designated by their index) must be drawn.
+
+We then have
+```c++
+// Defining vertices positions
+float positions[] = {
+    -0.5f, -0.5f,
+    0.5f, -0.5f,
+    0.5f, 0.5f,
+    -0.5f, 0.5f
+};
+
+// Defining vertices to be drawn
+unsigned int indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+```
+
+To create the index buffer in OpenGL, we pretty much do as we did to create the positions array buffer:
+
+```c++
+unsigned int ibo;
+glGenBuffers(1, &ibo);
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indices, GL_STATIC_DRAW);
+```
+
+In the rendering loop, we then replace the `glDrawArray` with a `glDrawElements` call:
+```c++
+glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+```
+
+> index buffer elements **MUST** always be unsigned (u char, u int, ...)
---
+++ 09/src/main.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file main.cpp
+ * @author Adrien Lucbert (adrien.lucbert@epitech.eu)
+ * @brief main file
+ * @version 0.1
+ * @date 2020-04-26
+ */
+
+#define ERRCODE (-1)
+#define SUCCODE (0)
+
+#include <iostream>
+#include "Shader/Shader.hpp"
+#include "Wrappers/GLEW/Glew.hpp"
+#include "Wrappers/GLFW/Glfw.hpp"
+#include "Wrappers/GLFW/Window.hpp"
+
+int main(void)
+{
+    std::string content;
+
+    GLFW::Glfw glfw;
+    GLFW::Window window(640, 480, "Hello world");
+    GLEW::Glew glew;
+
+    // clang-format off
+    float positions[] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    // clang-format on
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    // index buffer object
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indices, GL_STATIC_DRAW);
+
+    Shader::programSource source = Shader::parse("res/shaders/basic.shader");
+    unsigned int shader = Shader::create(source.vertexSource, source.fragmentSource);
+    glUseProgram(shader);
+    while (!glfwWindowShouldClose(*window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glfwSwapBuffers(*window);
+        glfwPollEvents();
+    }
+    glDeleteProgram(shader);
+    return 0;
+}
